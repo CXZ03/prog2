42d4afdf1a7f4b4c047083daa0a806e148c0fdcb
cxz03 <chengxz0393@gmail.com>
2025-03-14 13:57:13 +0100

    define basic code structure

---
+++ Practica/src/practica/game/Tablero.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package practica.game;
+
+import javax.swing.JPanel;
+
+/**
+ * Esta clase se colocará las piezas del tablero
+ * @author cxz03
+ */
+public class Tablero extends JPanel{
+    
+}
---
+++ Practica/src/practica/gui/Ventana.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package practica.gui;
+
+import javax.swing.JFrame;
+
+/**
+ * Esta clase será un JFrame y funcionará como el contenedor del tablero
+ * @author cxz03
+ */
+public class Ventana extends JFrame{
+    
+}
